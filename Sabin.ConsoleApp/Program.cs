147e68c92f407cc4386c85a5619053c37ac9e2ce
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2021-08-31 19:07:21 +0545

    Add project files.

---
+++ Sabin.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sabin.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
