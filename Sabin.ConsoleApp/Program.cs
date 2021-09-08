43dd909e7b6a33ea0113dbd42ffc90ea3f5e11af
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2021-09-08 20:29:39 +0545

    classes and string concatenation explained

--- Sabin.ConsoleApp/Program.cs
+++ Sabin.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Sabin.ConsoleApp
 {
@@ -6,7 +7,79 @@ namespace Sabin.ConsoleApp
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello! World");
+            var res = "n";
+            do
+            {
+                //StringConcatenationExample();
+
+                ClassExample();
+
+                Console.WriteLine("Want to continue more (y/n)?");
+                res = Console.ReadLine();
+            } while (res.ToLower() == "y");
+            Console.ReadLine();
+        }
+
+        private static void ClassExample()
+        {
+            //object are instance of class
+            //after instansiation of class, object are born/created
+            StudentMarks s1 = new StudentMarks();
+            StudentMarks s2 = new StudentMarks();
+            StudentMarks s3 = new StudentMarks();
+            HondaUnicorn150CC sabinkobike = new HondaUnicorn150CC();
+            sabinkobike.OwnerName = "Sabin";
+            sabinkobike.Color = "Black";
+            HondaUnicorn150CC chandankobike = new HondaUnicorn150CC();
+            chandankobike.OwnerName = "Chandan";
+            chandankobike.Color = "Grey";
+
+            sabinkobike.Start();
+            chandankobike.Start();
+
+            sabinkobike.GetInfo();
+            chandankobike.GetInfo();
+        }
+
+        private static void StringConcatenationExample()
+        {
+            //basic concatenation
+            string s1 = "Hello!";
+            string s2 = "World";
+            string s3 = s1 + " " + s2;
+
+            //string formatting
+            string s4 = "{0} {1} ({0},{1})";
+            string s5 = string.Format(s4, "Sabin", "Bhusal"); //Sabin Bhusal (Sabin,Bhusal)
+
+            Console.WriteLine("With String format Method");
+            Console.WriteLine("Enter your name:");
+            var name = Console.ReadLine();
+            var ran = new Random();
+            var number = ran.Next(10000, 99999);
+            string template = "Hi {0}, Your OTP is {1}.";
+            string message = string.Format(template, name, number);
+            Console.WriteLine(message);
+            Console.WriteLine("------------------------------------");
+
+            //string interpolation
+            Console.WriteLine("With String Interpolation Method");
+            Console.WriteLine("Enter your name:");
+            name = Console.ReadLine();
+            ran = new Random();
+            number = ran.Next(10000, 99999);
+            string s6 = $"Hi {name}, Your OTP is {number}";
+            Console.WriteLine(s6);
+
+            //string builder
+            var sb = new StringBuilder();
+            sb.AppendLine("Hi,");
+            sb.AppendLine("The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. ");
+            sb.AppendLine("The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. ");
+
+            sb.AppendLine("The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. ");
+            var result = sb.ToString();
+            Console.WriteLine(result);
         }
     }
 }
---
+++ Sabin.ConsoleApp/StudentMarks.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabin.ConsoleApp
+{
+    public class StudentMarks
+    {
+    }
+
+    public class HondaUnicorn150CC
+    {
+        public string OwnerName = "";
+        public string Color = "";
+
+        public void Start()
+        {
+            Console.WriteLine($"{OwnerName} ko Bike started");
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Owner = {OwnerName}");
+            Console.WriteLine($"Color = {Color}");
+        }
+    }
+}
